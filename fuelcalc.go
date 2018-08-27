e579988e581cb2dddf2770cb0d9f79f403668c05
Lex Barker <lex1903@gmail.com>
2018-08-27 21:31:39 +0100

    CLI for the fealcalc example

--- fuelcalc.go
+++ fuelcalc.go
@@ -1,17 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/lexbarker/fuelcalc"
+)
 
 //import "fmt"
 
 func main() {
-	miles := 300
-	var litres float64
-	//var litre float64
-	const l2gal = 4.53
-	fmt.Scan(&litres)
-	mpl := float64(miles) / float64(litres)
-	fmt.Printf("you achieved %f mile per litre\n", mpl)
-	mpg := mpl * l2gal
-	fmt.Printf("mpg  = %f\n", mpg)
+	var fc fuelcalc.Mpg
+	var fu fuelcalc.Fuelused
+	fmt.Printf("How many Miles?\n")
+	fmt.Scan(&fc.Miles)
+	fmt.Printf("\nHow many Litres ?\n")
+	fmt.Scan(&fc.Litres)
+	res := fc.Consumption()
+	fmt.Printf("MPG returned is %f\n", res)
+	fmt.Printf("How many Miles ? \n")
+	fmt.Scan(&fu.Miles)
+	fmt.Printf("\nwhat is the MPG?\n")
+	fmt.Scan(&fu.Mpg)
+	fu.Fuelrequired()
+	fmt.Printf("\nyou need %f litres", fu.Litres)
 }
